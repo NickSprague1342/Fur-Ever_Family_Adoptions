e503e05ff6e415fdd44358076461f9a35ddab3fd
Nick Sprague <n.sprague18@gmail.com>
2024-06-02 16:09:38 -0400

    added auth and apollo server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,32 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 3001
-const ( typedefs, resolvers ) = require('/Users/nicholassprague/bootcamp/PROJECTS/Project_3/server/schemas')
+const { apServer } = require ('@apollo/server')
+const { authMw } = require ('server/utils/auth.js')
+const {typedefs, resolvers} = require('/Users/nicholassprague/bootcamp/PROJECTS/Project_3/server/schemas')
 const apServer = new ApolloServer({
     typedefs,
     resolvers
 });
 
-const apolloStart = async() => {
+const apolloStart = async () => {
+    await server.start();
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+    app.use('/graphql', expressMiddleware(server, {
+        context: authMiddleware
+    }));
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.join(__dirname, '../client/dist')));
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(__dirname, 'server/models/index.js'));
+        });
+    }
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+        });
+    });
+};
 
-}
+apolloStart();
