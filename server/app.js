0884ca69b9556bcca95b1dffb3ec1ba993fc6782
Nick Sprague <n.sprague18@gmail.com>
2024-06-04 19:30:32 -0400

    finished app.js...maybe

--- server/app.js
+++ server/app.js
@@ -11,10 +11,38 @@ const app = express()
 
 
 
-connect (process.env.mongodb_uri, {
+connect (process.env.mongodb_URI, {
     newURL = true,
     uniTopo = true,
 })
 connection.running('error'. error) => {
     console.log(`Database Error`), error
-}
+};
+
+const server = new ApolloServer({
+    typedefs, resolvers,
+    context: ({req}) => {
+        const logToken = req.headers.auth = ''
+        const logUser = verifyLogin(logToken)
+        return{logUser}
+    }
+});
+
+server.startMw({ app, path: './graphql'})
+
+const pFApiKey = process.env.'pFApiKey'
+const pFApiSecret = process.env.'pFApiSecret'
+
+app.get('/getAnimals', async (req, res) => {
+    try {
+        const animals = await fetchAnimals(pFApiKey, pFApiSecret)
+        res.json(animals)
+    }catch (err) {
+        console.err('Oops! We may have encountered an error! Please try again later!', err)
+    }
+});
+
+const PORT = process.env || 3001
+app.listen(PORT, () => {
+    console.log (`Server running @ ${PORT}`)
+})
